44d1b51f792644281874ea09e28270e1b7c6798a
PrinceChauhanhub <princechauhan220103@gmail.com>
2025-08-07 11:55:55 +0530

    added threading feature to get previous chat conversations

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -5,25 +5,26 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.messages import HumanMessage
 
 load_dotenv()
 llm = ChatGoogleGenerativeAI(model = "gemini-1.5-flash")
 
 class ChatState(TypedDict):
     
-    message : Annotated[list[BaseMessage], add_messages()] # using reducer
+    messages : Annotated[list[BaseMessage], add_messages()] # using reducer
 
 def chat_node(state : ChatState):
     
     ##take user query from state
-    message = state['message']
+    messages = state['messages']
     
     # send to llm 
-    response = llm.invoke(message)
+    response = llm.invoke(messages)
     
     # response store state
     
-    return {'message': [response]}
+    return {'messages': [response]}
 
 checkpointer = InMemorySaver()
 
@@ -34,5 +35,3 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer = checkpointer)
-
-
